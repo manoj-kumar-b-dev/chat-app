cddc836db58a6e0f5af82cef389a32da8c83af36
manoj-kumar-b-dev <manojkumarb.2305@gmail.com>
2025-12-18 21:32:54 +0530

    file updated

--- src/Projects/Chat App/Components/Main.jsx
+++ src/Projects/Chat App/Components/Main.jsx
@@ -39,8 +39,8 @@ React.useEffect(()=>
     <div className='flex justify-center'>
       <main className="main w-110 h-screen relative bg-[rgba(59,250,6,1)] rounded-2xl relative">
 
-        <section className='mt-5 ml-5'>
-           <div>
+        <section className='mt-5 ml-5 '>
+           <div className='overflow-y-auto h-120'>
              {recievedMessage?.map((data)=><p key={data.id} className='bg-white mt-3 py-2 pl-4 rounded-lg w-100'>{data.text}</p>)}
            </div>
         </section>
@@ -50,7 +50,7 @@ React.useEffect(()=>
           <label htmlFor="fileInput">File</label>
           <input onKeyUp={e=>e.key==="Enter"?sendMessage():"null"} onChange={(e)=>setMessage(e.target.value)} className=' outline-none pl-5 py-2 w-80 bg-[white] h-12 rounded-3xl' placeholder='hello' type="text" />
           <button onClick={sendMessage} className='cursor-pointer '>
-            <img className=' rounded-3xl w-12 h-12 ml-2 h-8 object-cover' src="https://share.google/lAsRlAuHpKXsi4FSX" alt="" />
+            <img className=' rounded-3xl w-12 h-12 ml-2 h-8 object-cover' src="" alt="" />
           </button>
           
         </section>
---
+++ src/Projects/Portfolio/Components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <div>
+      Under Construction
+    </div>
+  )
+}
+
+export default Main
